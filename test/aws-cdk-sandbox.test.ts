3f78aa3a83b73719e26521d9fcc2f75ce89698a4
Tugce Konuklar <tugce.konuklar@idealo.de>
2022-08-04 15:19:42 +0200

    Initial commit

---
+++ test/aws-cdk-sandbox.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkSandbox from '../lib/aws-cdk-sandbox-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-sandbox-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkSandbox.AwsCdkSandboxStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
